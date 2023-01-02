df48cc19a8df416480da4745e0dcd0ce93975703
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-02 13:07:37 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,6 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import React, { Component } from 'react'
+
+export default class Coin extends Component {
+  render() {
+    return (
+      <div>This is a coin. Name: {this.props.name}</div>
+    )
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
