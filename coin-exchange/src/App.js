88987c78cdc713e2c635f3cf03bedc11bff1fab6
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-02 22:28:48 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coin Exchange {sum}</h1>
+        <h1>Coin Exchange </h1>
       </header>
       <table>
         <Thread>
