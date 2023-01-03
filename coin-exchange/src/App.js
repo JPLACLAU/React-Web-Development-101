3b4a782b515dd1d8763135c9b3b2a92b22c440bf
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-03 19:36:32 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Coin from "./components/Coin/Coin";
+import CoinList from "./components/CoinList/CoinList";
 import logo from "./logo.svg";
 import AccountBalance from "./components/AccountBalance/AccountBalance";
 import { uuid } from "uuidv4";
@@ -48,20 +48,7 @@ class App extends React.Component {
           <h1 className="App-title"> Coin Exchange </h1>
         </header>
         <AccountBalance amount={10000} />
-        <table className="coin-table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Ticker</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.coinData.map(({ name, ticker, price }) => (
-              <Coin key={ticker} name={name} ticker={ticker} price={price} />
-            ))}
-          </tbody>
-        </table>
+        <CoinList coinData={this.state.coinData} />
       </div>
     );
   }
