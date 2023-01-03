bf1cac95cf3dbcd74585b042f526502200ad29e7
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-02 22:02:22 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+let sum = 0;
+
+for (let num of [1, 2, 3, 4, 5]) {
+  sum += num;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coin Exchange </h1>
+        <h1>Coin Exchange {sum}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
