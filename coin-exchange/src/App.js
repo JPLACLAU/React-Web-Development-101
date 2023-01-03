fecaa2372ded91ef001fbb8820896a7457b6487a
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-03 13:30:03 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -4,6 +4,66 @@ import Coin from "./components/Coin/Coin";
 import logo from "./logo.svg";
 import AccountBalance from "./components/AccountBalance/AccountBalance";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance : 10000,
+      coinData: [
+        {
+          name:'Bitcoin',
+          ticker: 'BTC',
+          price:'16600.59',
+        },
+        {
+          name:'Ethereum',
+          ticker: 'ETH',
+          price:'1201.48',
+        },
+        {
+          name:'Tether',
+          ticker: 'USDT',
+          price:'1.0',
+        },
+        {
+          name:'Ripple',
+          ticker: 'XRP',
+          price:'0.2',
+        },
+   ]
+
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="React logo" className="App-logo" />
+          <h1 className="App-title"> Coin Exchange </h1>
+        </header>
+        <AccountBalance amount={10000} />
+        <table className="coin-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ticker</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody> 
+            <Coin name= {this.state.coinData[0].name }   ticker= {this.state.coinData[0].ticker } price= {this.state.coinData[0].price } />
+            <Coin name= {this.state.coinData[1].name }   ticker= {this.state.coinData[1].ticker } price= {this.state.coinData[1].price } />
+            <Coin name= {this.state.coinData[2].name }   ticker= {this.state.coinData[2].ticker } price= {this.state.coinData[2].price } />
+            <Coin name= {this.state.coinData[3].name }   ticker= {this.state.coinData[3].ticker } price= {this.state.coinData[3].price } />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -30,5 +90,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
--- coin-exchange/src/components/AccountBalance/AccountBalance.jsx
+++ coin-exchange/src/components/AccountBalance/AccountBalance.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Section1 = styled.section`
-  border: 1px solid red;
   font-size: 2rem;
   tex-align: left;
   padding: 1.5rem 0 1.5rem 5rem;
