173f83a04c05b016577d91124169045904eb722d
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-02 22:26:47 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -8,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Coin Exchange {sum}</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <table>
         <Thread>
