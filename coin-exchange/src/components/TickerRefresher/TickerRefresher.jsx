0596e4047f524bc64020da9a83f7aaa12d66640f
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-07 13:25:26 -0300

    z

---
+++ coin-exchange/src/components/TickerRefresher/TickerRefresher.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+export default class TickerRefresher extends Component {
+  render() {
+    return <div>TickerRefresher</div>;
+  }
+}
