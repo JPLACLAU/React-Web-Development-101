2a16ee0ec64325d45df66583f0a812cfdc930181
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-07 14:31:07 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -55,15 +55,10 @@ class App extends React.Component {
   };
   componentDidMount = async () => {
     let response = await axios.get("https://api.coinpaprika.com/v1/coins");
-    let coinData = response.data.slice(0, COIN_COUNT).map(function (coin) {
-      return {
-        key: coin.id,
-        name: coin.name,
-        ticker: coin.symbol,
-        balance: 0,
-        price: 0,
-      };
-    });
+    let coinData = response.data.slice(0, COIN_COUNT).map((coin) => coin.key);
+    const tickerURL = "https://api.coinpaprika.com/v1/tickers/";
+    const promises = coinData.map(({ key }) => axios.get(ticketURL + key));
+    await Promise.all(promises);
 
     this.setState({ coinData });
   };
