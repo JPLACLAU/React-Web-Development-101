b76b9059708f9c43ac6da01be44ed04585a2a405
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-07 21:43:48 -0300

    z

--- coin-exchange/src/App.js
+++ coin-exchange/src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
     });
   };
   handleRefresh = async (valueChangeId) => {
-    const ticketURL = `https://api.coinpaprika.com/v1/tickers/${valueChangeId}`;
+    const ticketURL = `https://api.coinpaprika.com/v1/tickers/${valueChangeId}/`;
     const response = await axios.get(ticketURL);
     const newPrice = formatPrice(response.data.quotes.USD.price);
     const newCoinData = this.state.coinData.map(function (values) {
